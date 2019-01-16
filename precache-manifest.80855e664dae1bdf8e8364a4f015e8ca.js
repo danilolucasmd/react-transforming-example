5d46ce45cc72dd43bc4b79e80cad325719a1f37d
Moraes Dias <dsme@gft.com>
2019-01-16 09:53:33 -0200

    Initial build subtree commit

---
+++ precache-manifest.80855e664dae1bdf8e8364a4f015e8ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cf5f1c5db0ef214876ec",
+    "url": "/static/js/main.cf5f1c5d.chunk.js"
+  },
+  {
+    "revision": "49dab0b6530874ab4296",
+    "url": "/static/js/1.49dab0b6.chunk.js"
+  },
+  {
+    "revision": "cf5f1c5db0ef214876ec",
+    "url": "/static/css/main.e12b50b7.chunk.css"
+  },
+  {
+    "revision": "7030e5303be0a74f0ef9054d5ffbbc65",
+    "url": "/index.html"
+  }
+];
